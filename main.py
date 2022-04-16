e41caaa9be0d228444a09ccd483db4bb87d951f4
Shizuku-Asami <xv.chronos@gmail.com>
2022-04-16 21:12:22 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
